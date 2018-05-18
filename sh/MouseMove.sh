79a2e6477b7e7314bab93f539057df230a1ba04a
Albara Ramli <arramli@ucdavis.edu>
2018-05-18 05:58:50 -0700

    Update MouseMove.sh

--- sh/MouseMove.sh
+++ sh/MouseMove.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Move Mouse to Bottom Right Corner
-if [ "$(sed '3q;d' /home/pi/rpiMasjidDisplay/config.txt)" != 'mic' ]; then
+if [ "$(sed '3q;d' /home/pi/iqamah/config.txt)" != 'mic' ]; then
 xdotool mousemove 1080 1920
 fi
