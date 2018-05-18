e261a2230be57f1db351ec314c136e9c12740a39
Albara Ramli <arramli@ucdavis.edu>
2018-05-18 06:06:42 -0700

    Update screen_off.sh

--- sh/screen_off.sh
+++ sh/screen_off.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "0" > /home/pi/iqamah/data/sh/screen_status.txt
-if [ "$(sed '3q;d' /home/pi/iqamah/data/config.txt)" != 'mic' ]; then
+echo "0" > /home/pi/iqamah/sh/screen_status.txt
+if [ "$(sed '3q;d' /home/pi/iqamah/config.txt)" != 'mic' ]; then
 vcgencmd display_power 0
 sudo echo 1 > /sys/class/backlight/rpi_backlight/bl_power # LCD 7'' turn off with 1 tag
 fi
