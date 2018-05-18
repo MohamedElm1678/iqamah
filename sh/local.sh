159f653a60ed7529592ce68706d6f5040de991ae
Albara Ramli <arramli@ucdavis.edu>
2018-05-18 06:01:55 -0700

    Update local.sh

--- sh/local.sh
+++ sh/local.sh
@@ -1,3 +1,3 @@
-if [ "$(sed '3q;d' /home/pi/rpiMasjidDisplay/config.txt)" != 'mic' ]; then
-curl -L -o /home/pi/rpiMasjidDisplay/data/local.html "$(sed '4q;d' /home/pi/rpiMasjidDisplay/config.txt)local.php?city=$(sed '1q;d' /home/pi/rpiMasjidDisplay/config.txt)&icdx=1&note_show=0&remain_show=0&time_show=0&refresh_stop=1&arrow_show=0&date_show=0&current_show=0"
+if [ "$(sed '3q;d' /home/pi/iqamah/data/config.txt)" != 'mic' ]; then
+curl -L -o /home/pi/iqamah/data/data/local.html "$(sed '4q;d' /home/pi/iqamah/data/config.txt)local.php?city=$(sed '1q;d' /home/pi/iqamah/data/config.txt)&icdx=1&note_show=0&remain_show=0&time_show=0&refresh_stop=1&arrow_show=0&date_show=0&current_show=0"
 fi
