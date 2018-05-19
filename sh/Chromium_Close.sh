b651272415532da6edd479c5499b778c4bd0ffd5
Albara Ramli <arramli@ucdavis.edu>
2018-05-18 19:01:20 -0700

    Update Chromium_Close.sh

--- sh/Chromium_Close.sh
+++ sh/Chromium_Close.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "0" > /home/pi/iqamah/sh/chrome_status.txt
+echo "0" > /home/pi/iqamah/data/chrome_status.txt
 # Close Chromium Browser Instance, if the device is not mic
 if [ "$(sed '3q;d' /home/pi/iqamah/config.txt)" != 'mic' ]; then
   pkill chromium
