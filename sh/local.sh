a49889ae47d8a470339c41d12021912a4f595412
Albara Ramli <arramli@ucdavis.edu>
2018-05-19 16:41:10 -0700

    Update local.sh

--- sh/local.sh
+++ sh/local.sh
@@ -1,3 +1,17 @@
-if [ "$(sed '3q;d' /home/pi/iqamah/config.txt)" != 'mic' ]; then
-curl -L -o /home/pi/iqamah/data/local.html "$(sed '4q;d' /home/pi/iqamah/config.txt)local.php?city=$(sed '1q;d' /home/pi/iqamah/config.txt)&icdx=1&note_show=0&remain_show=0&time_show=0&refresh_stop=1&arrow_show=0&date_show=0&current_show=0"
+#!/bin/bash
+
+MY_PATH="`dirname \"$0\"`"              # relative
+MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
+if [ -z "$MY_PATH" ] ; then
+  # error; for some reason, the path is not accessible
+  # to the script (e.g. permissions re-evaled after suid)
+  exit 1  # fail
+fi
+#echo "$MY_PATH"
+CONFIG_FILE="$MY_PATH/../"config.txt
+DATA_PATH="$MY_PATH/../data/"
+LOCAL_PATH="$DATA_PATH"local.html
+
+if [ "$(sed '3q;d' "$CONFIG_FILE")" != 'mic' ]; then
+curl -L -o "$LOCAL_PATH" "$(sed '4q;d' "$CONFIG_FILE")local.php?city=$(sed '1q;d' "$CONFIG_FILE")&icdx=1&note_show=0&remain_show=0&time_show=0&refresh_stop=1&arrow_show=0&date_show=0&current_show=0"
 fi
