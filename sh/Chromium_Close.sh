d6ca81bd124ab162a9b17f51350a0743d76bd91f
Albara Ramli <arramli@ucdavis.edu>
2018-05-19 16:30:51 -0700

    Update Chromium_Close.sh

--- sh/Chromium_Close.sh
+++ sh/Chromium_Close.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-echo "0" > /home/pi/iqamah/data/chrome_status.txt
+
+MY_PATH="`dirname \"$0\"`"              # relative
+MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
+if [ -z "$MY_PATH" ] ; then
+  # error; for some reason, the path is not accessible
+  # to the script (e.g. permissions re-evaled after suid)
+  exit 1  # fail
+fi
+#echo "$MY_PATH"
+CONFIG_FILE="$MY_PATH/../"config.txt
+DATA_PATH="$MY_PATH/../data/"
+
+echo "0" > "$DATA_PATH"chrome_status.txt
 # Close Chromium Browser Instance, if the device is not mic
-if [ "$(sed '3q;d' /home/pi/iqamah/config.txt)" != 'mic' ]; then
+if [ "$(sed '3q;d' "$CONFIG_FILE")" != 'mic' ]; then
   pkill chromium
 fi
