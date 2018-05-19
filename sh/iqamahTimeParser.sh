161413caad2e36690a463b6213014d0e447f7149
Albara Ramli <arramli@ucdavis.edu>
2018-05-19 16:38:56 -0700

    Update iqamahTimeParser.sh

--- sh/iqamahTimeParser.sh
+++ sh/iqamahTimeParser.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
 
+MY_PATH="`dirname \"$0\"`"              # relative
+MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
+if [ -z "$MY_PATH" ] ; then
+  # error; for some reason, the path is not accessible
+  # to the script (e.g. permissions re-evaled after suid)
+  exit 1  # fail
+fi
+#echo "$MY_PATH"
+CONFIG_FILE="$MY_PATH/../"config.txt
+DATA_PATH="$MY_PATH/../data/"
+TMP1_PATH="$MY_PATH/../data/"tmp.txt
+TMP2_PATH="$MY_PATH/../data/"tmp2.txt
+CSV_PATH="$MY_PATH/../data/"parsedIqamah.csv
+
 #This sh runs every day once to parse Athan and Iqamah times from server, work only in monitor
-if [ "$(sed '3q;d' /home/pi/iqamah/config.txt)" != 'mic' ]; then
+if [ "$(sed '3q;d' "$CONFIG_FILE")" != 'mic' ]; then
 #Download Iqamah Times for Davis from Server
-wget -q "$(sed '4q;d' /home/pi/iqamah/config.txt)api.php?city=$(sed '1q;d' /home/pi/iqamah/config.txt)&id=0" -O /home/pi/iqamah/data/tmp.txt
+wget -q "$(sed '4q;d' "$CONFIG_FILE")api.php?city=$(sed '1q;d' "$CONFIG_FILE")&id=0" -O "$TMP1_PATH"
 
 #Extract Salat Name and Time from Text File
-awk -F'<BR>' '{for (i=1;i<=NF;i++) print $i}' /home/pi/iqamah/data/tmp.txt > /home/pi/iqamah/data/tmp2.txt
+awk -F'<BR>' '{for (i=1;i<=NF;i++) print $i}' "$TMP1_PATH" > "$TMP2_PATH"
 
 #Check Number of Ljnes in Temp File
-lineCheck=$(wc -l < /home/pi/iqamah/data/tmp2.txt)
+lineCheck=$(wc -l < "$TMP2_PATH")
 
 if [ $lineCheck = "19" ]
     then
 	#Remove ':' Symbol from the Extracted Time (for comparison purpose)
-	sed 's/://' /home/pi/iqamah/data/tmp2.txt > /home/pi/iqamah/data/parsedIqamah.csv
+	sed 's/://' "$TMP2_PATH" > "$CSV_PATH"
 fi
 
 #Delete all Temp. Files
-rm /home/pi/iqamah/data/tmp.txt
-rm /home/pi/iqamah/data/tmp2.txt
+rm "$TMP1_PATH"
+rm "$TMP2_PATH"
 
 #################### Other Functions/Ways that Didn't work ####################
 #Another way to remove : from the times
