191cc8ba7ee0c39f00e9fd1f85fceff3a068f3c8
Albara Ramli <arramli@ucdavis.edu>
2018-05-18 19:01:38 -0700

    Update Chromium_Open.sh

--- sh/Chromium_Open.sh
+++ sh/Chromium_Open.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "1" > /home/pi/iqamah/sh/chrome_status.txt
+echo "1" > /home/pi/iqamah/data/chrome_status.txt
 # Open Chromium Browser Instance, if the device is not mic
 if [ "$(sed '3q;d' /home/pi/iqamah/config.txt)" != 'mic' ]; then
   chromium-browser --kiosk --incognito --no-sandbox  --noerrdialogs -disable-session-crashed-bubble --disable-infobars "$(sed '4q;d' /home/pi/iqamah/config.txt)screen.php?size=$(sed '5q;d' /home/pi/iqamah/config.txt)&icdx=1&city=$(sed '1q;d' /home/pi/iqamah/config.txt)"
