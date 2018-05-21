ae0311965451f561fef699e86043d5688ca4975d
Albara Ramli <arramli@ucdavis.edu>
2018-05-20 18:13:03 -0700

    Update screenTimeCheck.py

--- py/screenTimeCheck.py
+++ py/screenTimeCheck.py
@@ -32,7 +32,7 @@ screenOffAfterDuha = Duha + timedelta(minutes=30)
 screenOffAfterDuha = screenOffAfterDuha.time()
 screenOnBeforeDhuhr = DhuhrAthan - timedelta(hours=1)
 screenOnBeforeDhuhr = screenOnBeforeDhuhr.time()
-screenOffAfterIsha = IshaIqamah + timedelta(hours=2)
+screenOffAfterIsha = IshaIqamah + timedelta(hours=1.7)
 screenOffAfterIsha = screenOffAfterIsha.time()
 
 # Define Midnight Time
@@ -43,60 +43,80 @@ Midnight = Midnight.time()
 # currentTime = datetime.combine(date.today(), time(12, 50))
 # currentTime = currentTime.time()
 
+'''
 # Print Prayer Times for Verifiation
-# print("screenOnBeforeFajr: " + str(screenOnBeforeFajr))
-# print("screenOffAfterDuha: " + str(screenOffAfterDuha))
-# print("screenOnBeforeDhuhr: " + str(screenOnBeforeDhuhr))
-# print("screenOffAfterIsha: " + str(screenOffAfterIsha))
-
+print("screenOnBeforeFajr: " + str(screenOnBeforeFajr))
+print("screenOffAfterDuha: " + str(screenOffAfterDuha))
+print("screenOnBeforeDhuhr: " + str(screenOnBeforeDhuhr))
+print("screenOffAfterIsha: " + str(screenOffAfterIsha))
+'''
 #while True:
 # Check Server Screen Status
 autoscreen_file=path_data+"autoscreen_status.txt"
 if os.path.exists(autoscreen_file):
     autoScreenStatusFile = open(autoscreen_file, "r")
     autoScreenStatus = autoScreenStatusFile.readline(1)
+    print "auto file exsist="+autoScreenStatus
 else:
     autoScreenStatus = "1"
-
+    print "auto file Not exsist="+autoScreenStatus
+'''
 screen_file=path_data+"screen_status.txt"
 if os.path.exists(screen_file):
     screenStatusFile = open(screen_file, "r")
     screenStatus = screenStatusFile.readline(1)
+    print "screen file exsist="+screenStatus
 else:
-    screenStatus = "1"
-    
+    screenStatus = "0"
+    print "screen file Not exsist="+screenStatus
+'''
 # If There is No Override, Run Screen Check Schedule
 if autoScreenStatus == "1":
     # Check Current Time
     currentTime = datetime.now().time()
-    # Check real time screenStatus from shell
-    ##output = subprocess.check_output("vcgencmd display_power", shell=True)
-    ##screenStatus = output[14]
-
+    if scree_size=="27":
+        # Check real time screenStatus from shell
+        output = subprocess.check_output("vcgencmd display_power", shell=True)
+        screenStatus = output[14]
+    if scree_size=="7":
+        output2 = subprocess.check_output("cat /sys/class/backlight/rpi_backlight/bl_power", shell=True)
+        screenStatus = output2[0]
+        if output2[0]=="0":
+            screenStatus="1"
+        else:
+            screenStatus="0"
+    print "screen="+screenStatus
     # Turn Screen On for Fajr
-    if currentTime >= screenOnBeforeFajr and currentTime <= screenOffAfterDuha:
+    if currentTime >= screenOnBeforeFajr and currentTime < screenOffAfterDuha:
         if screenStatus == "0":
             os_system(path_sh+'screen_on.sh')
+            print "ON"
 
     # Turn Screen Off After Fajr
-    if currentTime > screenOffAfterDuha and currentTime <= screenOnBeforeDhuhr:
+    if currentTime >= screenOffAfterDuha and currentTime < screenOnBeforeDhuhr:
         if screenStatus == "1":
             os_system(path_sh+'screen_off.sh')
+            print "OFF"
 
     # Turn Screen On Before Dhuhr
-    if currentTime > screenOnBeforeDhuhr and currentTime <= screenOffAfterIsha:
+    if currentTime >= screenOnBeforeDhuhr and currentTime < screenOffAfterIsha:
+        print "HERE"
         if screenStatus == "0":
             os_system(path_sh+'screen_on.sh')
+            print "ON"
 
     # Turn Screen Off After Isha
-    if currentTime > screenOffAfterIsha:
+    if currentTime >= screenOffAfterIsha:
         if screenStatus == "1":
             os_system(path_sh+'screen_off.sh')
+            print "OFF"
 
     # Turn Screen Off for the Night
     if currentTime < screenOnBeforeFajr:
         if screenStatus == "1":
             os_system(path_sh+'screen_off.sh')
+            print "OFF"
 
+print str(currentTime)+"="+str(screenOnBeforeDhuhr)+" "+str(currentTime) +"="+ str(screenOffAfterIsha)
 # Sleep for a second before rerunning the loop
 #timeSleep.sleep(1)
